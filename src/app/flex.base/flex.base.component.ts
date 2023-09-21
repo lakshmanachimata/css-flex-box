f28ce354af3f19406f9624b3b3d75399d2e0b68a
shodhsb <shodh1991@gmail.com>
2023-09-21 21:03:36 +0530

    Added css

--- src/app/flex.base/flex.base.component.ts
+++ src/app/flex.base/flex.base.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @Component({
-  selector: 'app-flex.base',
-  templateUrl: './flex.base.component.html',
-  styleUrls: ['./flex.base.component.scss']
+  selector: "app-flex.base",
+  templateUrl: "./flex.base.component.html",
+  styleUrls: ["./flex.base.component.scss"],
 })
 export class FlexBaseComponent {
-
-  constructor(private router : Router){}
-  title = 'css-flex-box';
-  cssFelxJCTypes = ["flex-start", "flex-end","center", "space-between","space-around","space-evenly"]
-  cssFelxAITypes = [ "stretch", "flex-start" , "flex-end" , "center" , "space-between","space-around","space-evenly" ,"baseline"]
+  constructor(private router: Router) {}
+  title = "css-flex-box";
+  cssFelxJCTypes = [
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+  ];
+  cssFelxAITypes = [
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+    "baseline",
+  ];
 
   // gotoDetailsJC(cfType : String) {
   //   this.router.navigateByUrl("/flexitype?flexstyle=" + cfType + "&type=justify-content")
@@ -20,5 +35,52 @@ export class FlexBaseComponent {
   // gotoDetailsAI(cfType : String) {
   //   this.router.navigateByUrl("/flexitype?flexstyle=" + cfType+ "&type=align-content")
   // }
-}
 
+  gridView = true;
+  tableView = false;
+
+  cardContent = [
+    {
+      id: 1,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+    {
+      id: 2,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+    {
+      id: 3,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+    {
+      id: 4,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+    {
+      id: 5,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+    {
+      id: 6,
+      type: "REST API",
+      name: "BoAApie-Agent",
+      dateModified: "Aug 24, 2023",
+      rating: 5,
+    },
+  ];
+}
