04bfa9d1e162d6aa674eb48ca3d8b8816b92d146
Your Name <you@example.com>
2023-09-23 22:49:21 +0530

    link color updated

--- src/app/api-details/api-details.component.ts
+++ src/app/api-details/api-details.component.ts
@@ -7,19 +7,21 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class ApiDetailsComponent {
   apiDetailsData : any = {}
-
+  viewJSON : boolean = false
+  apiDetailsString: string = ""
   constructor(
     public dialogRef: MatDialogRef<ApiDetailsComponent>,
     //@Optional() is used to prevent error if no data is passed
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
     this.apiDetailsData = {...data};
+    this.apiDetailsString = JSON.stringify(this.apiDetailsData)
   }
 
   setApiDetailsData(apiDetails: any) {
     this.apiDetailsData = apiDetails
   }
   viewAPIJSON() {
-
+    this.viewJSON = !this.viewJSON;
   }
   downloadAPIJSON() {
 
