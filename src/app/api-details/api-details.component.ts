4a2483acc65c0d2024499a7547a62068eb4a7026
Your Name <you@example.com>
2023-10-04 06:34:28 +0530

    added req changes

--- src/app/api-details/api-details.component.ts
+++ src/app/api-details/api-details.component.ts
@@ -1,18 +1,19 @@
 import { Component, Inject, Optional } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FileInfo } from '../interface/Fileinfo';
 @Component({
   selector: 'app-api-details',
   templateUrl: './api-details.component.html',
   styleUrls: ['./api-details.component.css'],
 })
 export class ApiDetailsComponent {
-  apiDetailsData : any = {}
+  apiDetailsData? : FileInfo
   viewJSON : boolean = false
   apiDetailsString: string = ""
   constructor(
     public dialogRef: MatDialogRef<ApiDetailsComponent>,
     //@Optional() is used to prevent error if no data is passed
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: FileInfo) {
     this.apiDetailsData = {...data};
     this.apiDetailsString = JSON.stringify(this.apiDetailsData)
   }
